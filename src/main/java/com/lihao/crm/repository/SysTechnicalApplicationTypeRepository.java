f54b4ba45cf7f029aaea68e9a1ee9476224e4f49
lihao <lihao@DESKTOP-942O117>
2018-04-27 17:58:38 +0800

    a

---
+++ src/main/java/com/lihao/crm/repository/SysTechnicalApplicationTypeRepository.java
@@ -0,0 +1,6 @@
+package com.lihao.crm.repository;
+
+import com.lihao.crm.entity.SysTechnicalApplicationType;
+
+public interface SysTechnicalApplicationTypeRepository extends BaseRepository<SysTechnicalApplicationType, Long> {
+}
