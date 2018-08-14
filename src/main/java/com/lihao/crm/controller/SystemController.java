0d400d0c58b93353b28e0e78b64a67baf0239757
lihao8173 <lihao198262@163.com>
2018-08-14 10:36:53 +0800

    修改内容
    1.去掉日志

--- src/main/java/com/lihao/crm/controller/SystemController.java
+++ src/main/java/com/lihao/crm/controller/SystemController.java
@@ -1,18 +1,14 @@
 package com.lihao.crm.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SystemController {
-	private static final Logger logger = LoggerFactory.getLogger(SystemController.class);
 
 	@GetMapping("/login")
 	private String login(Boolean error, Model model) {
-		logger.info("message error=" + error);
 		if(error != null && error == true) {
 			model.addAttribute("loginError", true);
 		}
